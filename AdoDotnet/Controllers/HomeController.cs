cd4bc3d783d75f981f2884091b454b0290d6b07a
yawar2019 <yawarali17@gmail.com>
2022-06-15 07:00:20 +0530

    committed

---
+++ AdoDotnet/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdoDotnet.Models;
+namespace AdoDotnet.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            var result = db.GetEmployees();
+            return View(result);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            int result = db.SaveEmployee(emp);
+            if (result > 0) {
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.GetEmployeeById(id);
+            return View(emp);
+        }
+    }
+}
---
+++ AdoDotnet/Models/EmployeeContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+namespace AdoDotnet.Models
+{
+    public class EmployeeContext
+    {
+        SqlConnection con = new SqlConnection("Data Source=AZAM-PC\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true");
+
+        public List<EmployeeModel> GetEmployees()
+        {
+            List<EmployeeModel> listEmployees = new List<EmployeeModel>();
+            SqlCommand cmd = new SqlCommand("sp_employee",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                EmployeeModel emp = new EmployeeModel();
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+
+                listEmployees.Add(emp);
+            }
+            return listEmployees;
+        }
+
+        public int SaveEmployee(EmployeeModel emp)
+        {
+            SqlCommand cmd = new SqlCommand("spr_insertEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@EmpName", emp.EmpName);
+            cmd.Parameters.AddWithValue("@EmpSalary", emp.EmpSalary);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            
+            con.Close();
+
+            return i;
+        }
+
+       public EmployeeModel GetEmployeeById(int ? id)
+        {
+            EmployeeModel emp = new EmployeeModel();
+            SqlCommand cmd = new SqlCommand("spr_getEmployeeDetailsbyId", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@empId",id);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+
+                 
+            }
+            return emp;
+        }
+    }
+}
